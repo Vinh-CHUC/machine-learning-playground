ec347e6e2fed458f34ff526a77cc72e3cb840443
Vinh CHUC <vinh.chuc@gmail.com>
2018-02-25 20:20:15 +0000

    Stuff

--- MNIST/perceptron.py
+++ MNIST/perceptron.py
@@ -1,8 +1,10 @@
-from sklearn.linear_model import LogisticRegression
 from keras.datasets import mnist
+import tensorflow as tf
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-lr = LogisticRegression()
+n_input = x_train.shape[1] * x_train.shape[2]
+
+lr = LogisticRegression(verbose=True)
 lr.fit([x.reshape(-1) for x in x_train], y_train)
 lr.score([x.reshape(-1) for x in x_test], y_test)
